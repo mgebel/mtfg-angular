7c3f3c3ccbe6c6fb23c3bf5b80c7d0d08da900a8
Martin Gebel <magebel@gmx.de>
2022-02-13 10:49:07 +0100

    add initial website content

---
+++ src/app/core/pages/project-website/project-website.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProjectWebsiteComponent } from './project-website.component';
+
+describe('ProjectWebsiteComponent', () => {
+  let component: ProjectWebsiteComponent;
+  let fixture: ComponentFixture<ProjectWebsiteComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProjectWebsiteComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProjectWebsiteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/core/pages/project-website/project-website.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-project-website',
+  templateUrl: './project-website.component.html',
+  styleUrls: ['./project-website.component.scss']
+})
+export class ProjectWebsiteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
